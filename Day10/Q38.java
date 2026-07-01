2672f8222f7caf0f81fe5793ac3a0257d2b490a9
GirdharRoy63 <25110cn289@glbitm.ac.in>
2026-07-01 20:05:23 +0530

    Create Q38.java

---
+++ Day10/Q38.java
@@ -0,0 +1,22 @@
+import java.io.*;
+class Q38
+{
+    public static void main(String as[])throws Exception 
+    {
+        DataInputStream ob=new DataInputStream(System.in);
+        int n;
+        System.out.println("Enter no. of rows : ");
+        n=Integer.parseInt(ob.readLine());
+        for(int i=n;i>=1;i--)
+        {
+            for(int j=n;j>i;j--)
+                 System.out.print(" ");
+          for(int j=1;j<=i;j++)
+            System.out.print("*");
+          for(int j=1;j<i;j++) 
+             System.out.print("*");
+          System.out.println(); 
+        }
+    }
+}
+        
