3a19fd9d66fe2e282bba03996d16ae3a08c9cd77
GirdharRoy63 <25110cn289@glbitm.ac.in>
2026-07-01 20:17:58 +0530

    Create Q39.java

---
+++ Day10/Q39.java
@@ -0,0 +1,21 @@
+import java.io.*;
+class Q39
+{
+    public static void main(String as[])throws Exception 
+    {
+        DataInputStream ob=new DataInputStream(System.in);
+        int n;
+        System.out.println("Enter no. of rows : ");
+        n=Integer.parseInt(ob.readLine());
+        for(int i=1;i<=n;i++)
+        {
+          for(int j=n;j>i;j--)
+            System.out.print(" ");
+          for(int j=1;j<=i;j++) 
+             System.out.print(j);
+          for(int j=i-1;j>=1;j--)
+             System.out.print(j);
+          System.out.println(); 
+        }
+    }
+}
