95ba9cb98d2554da7d047bcaf8935a99a6b89c3b
GirdharRoy63 <25110cn289@glbitm.ac.in>
2026-07-02 11:49:21 +0530

    Create Q22.java

---
+++ Day6/Q22.java
@@ -0,0 +1,52 @@
+import java.io.*;
+class Q22
+ {
+  public static void main(String as[])throws Exception
+   {
+    DataInputStream ob=new DataInputStream(System.in);
+    String str1="",str2="",str3="";
+    int i,k,num1,num2,sum1=0,hold1=0,hold2=-1;
+    double sum2=0.0;
+    System.out.println("Enter a Binary Number : ");
+    str1=ob.readLine();
+    i=str1.indexOf(".");
+    for(k=0;k<i;k++)
+    {
+        char c=str1.charAt(k);
+        if(!(c=='0'||c=='1'))
+        {
+        System.out.println("Entered number is not binary ");
+        break;
+        }
+    }
+    if(k==i)
+   {
+    for(int j=0;j<i;j++)
+    {
+        char c=str1.charAt(j);
+        str2=str2+c;
+    }
+   num1=Integer.parseInt(str2);
+   while(num1>0)
+   {
+    int j=num1%10;
+    sum1=sum1+j*(int)(Math.pow(2,hold1++));
+    num1=num1/10;
+   }
+   for(int j=i+1;j<str1.length();j++)
+    {
+        char c=str1.charAt(j);
+        str3=str3+c;
+    }
+    num2=Integer.parseInt(str3);
+    hold2=-((""+num2).length());
+    while(num2>0)
+    {
+        int j=num2%10;
+        sum2=sum2+j*(Math.pow(2,hold2++));
+        num2=num2/10;
+    }
+   System.out.println("Corresponding Decimal number is : "+(sum1+sum2));
+   }
+  }
+ }
