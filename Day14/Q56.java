0a1048066040dadf568a49b64257fea2b0eab867
GirdharRoy63 <25110cn289@glbitm.ac.in>
2026-07-03 20:33:39 +0530

    Create Q56.java

---
+++ Day14/Q56.java
@@ -0,0 +1,46 @@
+import java.io.*;
+class Q56
+ {
+  public static void main(String as[])throws Exception
+   {
+     DataInputStream ob=new DataInputStream(System.in);
+     int n,hold=0,pos=1;
+     System.out.println("Enter size of Array : ");
+     n=Integer.parseInt(ob.readLine());
+     int ar[]=new int[n];
+     int ar2[]=new int[n*n];
+     System.out.println("Enter Array elements : ");
+     for(int i=0;i<n;i++)
+        ar[i]=Integer.parseInt(ob.readLine());
+    System.out.println("Duplicate elment is : ");
+    for(int i=0;i<n;i++)
+    {
+        for(int j=i+1;j<n;j++)
+              {
+                if(ar[i]==ar[j])
+                  ar2[hold++]=j;
+              }
+    }
+    for(int i=0;i<n;i++)
+    {
+      for(int j=i+1;j<n;j++)
+        {
+            if(ar[i]==ar[j])
+           {
+             for(int k=0;k<hold;k++)
+               {
+                if(i==ar2[k])
+                    pos=0;
+               }
+           }
+       }
+       if(pos==0)
+       {
+           pos=1;
+          break;
+       }
+       else
+          System.out.println(ar[i]);
+    }
+  }
+}
